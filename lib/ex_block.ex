185d74d6f16c336b32cf3fb6c1e68a8b34a63e6d
Ben Glasser <ben@benglasser.com>
2024-01-15 10:00:00 -0700

    Initial Elixir project setup

---
+++ lib/ex_block.ex
@@ -0,0 +1,12 @@
+defmodule ExBlock do
+  @moduledoc """
+  A simple blockchain implementation in Elixir.
+  """
+
+  @doc """
+  Hello world function for initial setup.
+  """
+  def hello do
+    :world
+  end
+end

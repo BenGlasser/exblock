3833e359265ff5824a56ae9cd01e3fd61229a9a2
Ben Glasser <ben@benglasser.com>
2024-02-25 11:20:00 -0700

    Add put_hash function to crypto module

--- lib/crypto.ex
+++ lib/crypto.ex
@@ -24,6 +24,11 @@ defmodule Crypto do
     |> sha256
   end
 
+  @doc "Calculate and put the hash in the block"
+  def put_hash(%{} = block) do
+    %{ block | hash: hash(block) }
+  end
+
   # Calculate SHA256 for a binary string
   defp sha256(binary) do
     :crypto.hash(:sha256, binary) |> Base.encode16
